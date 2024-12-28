516e91e6972f728fb1e296ad83e91d13b50f7b93
Justin Beckwith <justin.beckwith@gmail.com>
2024-12-27 20:54:22 -0800

    add CI and linting

--- src/api.ts
+++ src/api.ts
@@ -1,115 +1,114 @@
 export const basePath = '/xled/v1';
 export const paths = {
-  ECHO: '/echo',
-  LOGIN: `/login`,
-  VERIFY: '/verify',  
-  GET_DEVICE_DETAILS: `/gestalt`,
-  GET_DEVICE_NAME: '/device_name',
-  GET_LED_CONFIG: `/led/config`,
-  GET_LED_EFFECTS: `/led/effects`,
-  GET_LED_OPERATION_MODE: `/led/mode`,
-  GET_LED_BRIGHTNESS: `/led/out/brightness`,
-  GET_LAYOUT: '/led/layout/full',
-  GET_CURRENT_LED_EFFECT: '/led/effects/current',
-  GET_NETWORK_STATUS: '/network/status',
-  SET_NETWORK_STATUS: '/network/status',
-  SET_LED_COLOR: '/led/color',
-  SET_LED_OPERATION_MODE: '/led/mode',
-  SET_DEVICE_NAME: '/device_name',
-  GET_MOVIES: '/movies',
-  GET_CURRENT_MOVIE: '/led/movies/current',
-  GET_SUMMARY: '/summary',
-  GET_LED_COLOR: '/led/color'
-}
+	ECHO: '/echo',
+	LOGIN: '/login',
+	VERIFY: '/verify',
+	GET_DEVICE_DETAILS: '/gestalt',
+	GET_DEVICE_NAME: '/device_name',
+	GET_LED_CONFIG: '/led/config',
+	GET_LED_EFFECTS: '/led/effects',
+	GET_LED_OPERATION_MODE: '/led/mode',
+	GET_LED_BRIGHTNESS: '/led/out/brightness',
+	GET_LAYOUT: '/led/layout/full',
+	GET_CURRENT_LED_EFFECT: '/led/effects/current',
+	GET_NETWORK_STATUS: '/network/status',
+	SET_NETWORK_STATUS: '/network/status',
+	SET_LED_COLOR: '/led/color',
+	SET_LED_OPERATION_MODE: '/led/mode',
+	SET_DEVICE_NAME: '/device_name',
+	GET_MOVIES: '/movies',
+	GET_CURRENT_MOVIE: '/led/movies/current',
+	GET_SUMMARY: '/summary',
+	GET_LED_COLOR: '/led/color',
+};
 
 const TypedKeys = <T extends object>(obj: T): (keyof T)[] => {
-  return Object.keys(obj) as (keyof T)[];
+	return Object.keys(obj) as (keyof T)[];
 };
 
 interface TokenData {
-  authentication_token: string;
+	authentication_token: string;
 }
 
 let tokenData: TokenData;
 
 export async function getLEDConfig() {
-  return await request(paths.GET_LED_CONFIG);
+	return await request(paths.GET_LED_CONFIG);
 }
-interface LEDColor{
-  hue: number,
-  saturation: number,
-  value: number,
-  red: number,
-  green: number,
-  blue: number,
-  code: number
+interface LEDColor {
+	hue: number;
+	saturation: number;
+	value: number;
+	red: number;
+	green: number;
+	blue: number;
+	code: number;
 }
 export async function getLEDColor(): Promise<LEDColor> {
-  return await request(paths.GET_LED_COLOR);
+	return await request(paths.GET_LED_COLOR);
 }
 
-
 export async function getSummary() {
-  return await request(paths.GET_SUMMARY);
+	return await request(paths.GET_SUMMARY);
 }
 
 export async function getCurrentMovie() {
-  return await request(paths.GET_CURRENT_MOVIE);
+	return await request(paths.GET_CURRENT_MOVIE);
 }
 
 export async function getMovies() {
-  return await request(paths.GET_MOVIES);
+	return await request(paths.GET_MOVIES);
 }
 
 export async function echo(body: unknown) {
-  return await request(paths.ECHO, {
-    method: 'POST',
-    body,
-  });
+	return await request(paths.ECHO, {
+		method: 'POST',
+		body,
+	});
 }
 
 export async function getDeviceName() {
-  return await request(paths.GET_DEVICE_NAME);
+	return await request(paths.GET_DEVICE_NAME);
 }
 
 export async function setDeviceName(name: string) {
-  return await request(paths.SET_DEVICE_NAME, {
-    method: 'POST',
-    body: {
-      name,
-    }
-  })
+	return await request(paths.SET_DEVICE_NAME, {
+		method: 'POST',
+		body: {
+			name,
+		},
+	});
 }
 
 export interface DeviceDetails {
-  /**
-   * (string) Twinkly
-   */
-  product_name: string;
-  /**
-   * (numeric string), e.g. “6”
-   */
-  hardware_version: string;
-  bytes_per_led: number;
-  hw_id: string;
-  flash_size: number;
-  led_type: number;
-  product_code: string;
-  fw_family: string;
-  device_name: string;
-  uptime: string;
-  mac: string;
-  uuid: string;
-  max_supported_led: number;
-  number_of_led: number;
-  led_profile: string;
-  frame_rate: number;
-  measured_frame_rate: number;
-  movie_capacity: number;
-  max_movies: number;
-  wire_type: number;
-  copyright: string;
-  code: number;
+	/**
+	 * (string) Twinkly
+	 */
+	product_name: string;
+	/**
+	 * (numeric string), e.g. “6”
+	 */
+	hardware_version: string;
+	bytes_per_led: number;
+	hw_id: string;
+	flash_size: number;
+	led_type: number;
+	product_code: string;
+	fw_family: string;
+	device_name: string;
+	uptime: string;
+	mac: string;
+	uuid: string;
+	max_supported_led: number;
+	number_of_led: number;
+	led_profile: string;
+	frame_rate: number;
+	measured_frame_rate: number;
+	movie_capacity: number;
+	max_movies: number;
+	wire_type: number;
+	copyright: string;
+	code: number;
 }
 
 /**
@@ -117,49 +116,49 @@ export interface DeviceDetails {
  * @param ip The IP address of the device
  */
 export function init(ip: string) {
-  for (const key of TypedKeys(paths)) {
-    paths[key] = `http://${ip}${basePath}${paths[key]}`;
-  }
+	for (const key of TypedKeys(paths)) {
+		paths[key] = `http://${ip}${basePath}${paths[key]}`;
+	}
 }
 
 /**
  * Gets information detailed information about the device.
  * Since firmware version 1.99.18.
- * @returns 
+ * @returns
  */
 export async function getDeviceDetails(): Promise<DeviceDetails> {
-  return await request(paths.GET_DEVICE_DETAILS);
+	return await request(paths.GET_DEVICE_DETAILS);
 }
 
 export async function getLEDEffects() {
-  return await request(paths.GET_LED_EFFECTS);
+	return await request(paths.GET_LED_EFFECTS);
 }
 
 export async function getLEDOperationMode() {
-  return await request(paths.GET_LED_OPERATION_MODE);
+	return await request(paths.GET_LED_OPERATION_MODE);
 }
 
 export async function getLEDBrightness() {
-  return await request(paths.GET_LED_BRIGHTNESS);
+	return await request(paths.GET_LED_BRIGHTNESS);
 }
 
 export async function getLayout() {
-  return await request(paths.GET_LAYOUT);
+	return await request(paths.GET_LAYOUT);
 }
 
 export async function getCurrentLEDEffect() {
-  return await request(paths.GET_CURRENT_LED_EFFECT);
+	return await request(paths.GET_CURRENT_LED_EFFECT);
 }
 
 export async function setLEDColor(red: number, green: number, blue: number) {
-  return await request(paths.SET_LED_COLOR, {
-    method: 'POST',
-    body: {
-      red,
-      green,
-      blue,
-    }
-  });
+	return await request(paths.SET_LED_COLOR, {
+		method: 'POST',
+		body: {
+			red,
+			green,
+			blue,
+		},
+	});
 }
 
 /**
@@ -170,16 +169,16 @@ export async function setLEDColor(red: number, green: number, blue: number) {
  * rt - receive effect in real time
  * effect - plays effect with effect_id
  * playlist - plays a movie from a playlist. Since firmware version 2.5.6.
- * @param {*} mode 
- * @returns 
+ * @param {*} mode
+ * @returns
  */
 export async function setLEDOperationMode(mode: string) {
-  return await request(paths.SET_LED_OPERATION_MODE, {
-    method: 'POST',
-    body: {
-      mode,
-    }
-  });
+	return await request(paths.SET_LED_OPERATION_MODE, {
+		method: 'POST',
+		body: {
+			mode,
+		},
+	});
 }
 
 /**
@@ -188,46 +187,47 @@ export async function setLEDOperationMode(mode: string) {
  * @returns The token data
  */
 async function getTokenData() {
-  const challengeBits = new Uint8Array(32);
-  crypto.getRandomValues(challengeBits);
-  const challenge = btoa(String.fromCharCode(...challengeBits));
-  const res = await fetch(paths.LOGIN, {
-    method: 'POST',
-    body: JSON.stringify({challenge}),
-  });
-  throwIfErr(res);
-  const data = await res.json();
-  return data;
+	const challengeBits = new Uint8Array(32);
+	crypto.getRandomValues(challengeBits);
+	const challenge = btoa(String.fromCharCode(...challengeBits));
+	const res = await fetch(paths.LOGIN, {
+		method: 'POST',
+		body: JSON.stringify({ challenge }),
+	});
+	throwIfErr(res);
+	const data = await res.json();
+	return data;
 }
 
 function throwIfErr(res: Response) {
-  if (!res.ok) {
-    throw new Error(`HTTP error! status: ${res.status}`);
-  }
+	if (!res.ok) {
+		throw new Error(`HTTP error! status: ${res.status}`);
+	}
 }
 
 /**
  * Make a request to the API automatically injecting the token if needed
- * @param {*} url 
- * @param {*} options 
+ * @param {*} url
+ * @param {*} options
  * @returns The data from the successful request
  */
+
+// biome-ignore lint/suspicious/noExplicitAny: <explanation>
 async function request(url: string, options: any = {}) {
-  if (!tokenData) {
-    tokenData = await getTokenData();
-    await request(paths.VERIFY, { method: 'POST' });
-  }
+	if (!tokenData) {
+		tokenData = await getTokenData();
+		await request(paths.VERIFY, { method: 'POST' });
+	}
 
-  options = options ?? {};
-  options.headers = options.headers ?? {};
-  options.headers['X-Auth-Token'] = tokenData.authentication_token;
+	options.headers = options.headers ?? {};
+	options.headers['X-Auth-Token'] = tokenData.authentication_token;
 
-  if (typeof options.body === 'object') {
-    options.body = JSON.stringify(options.body);
-  }
+	if (typeof options.body === 'object') {
+		options.body = JSON.stringify(options.body);
+	}
 
-  const res = await fetch(url, options);
-  throwIfErr(res);
-  const data = await res.json();
-  return data;
+	const res = await fetch(url, options);
+	throwIfErr(res);
+	const data = await res.json();
+	return data;
 }
