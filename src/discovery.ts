b0c68a571ccd0549ba5f66f1b275b269437c23e6
Justin Beckwith <justin.beckwith@gmail.com>
2025-01-02 17:57:23 -0800

    enable automatic broadcast address discovery (#31)

--- src/discovery.ts
+++ src/discovery.ts
@@ -1,11 +1,12 @@
 import { Buffer } from 'node:buffer';
 import dgram from 'node:dgram';
+import os from 'node:os';
 
 const UDP_BROADCAST_PORT = 5555;
 
 interface DiscoverOptions {
-	timeout: number;
-	broadcastAddress: string;
+	timeout?: number;
+	broadcastAddress?: string;
 }
 
 interface Device {
@@ -20,18 +21,70 @@ interface Device {
  */
 export async function discover(options?: DiscoverOptions) {
 	const timeout = options?.timeout ?? 1000;
-	const broadcastAddress = options?.broadcastAddress ?? '10.0.0.255';
+
+	// If the user sent a broadcast address, use it directly
+	if (options?.broadcastAddress) {
+		return await sendDiscoveryRequest(options.broadcastAddress, timeout);
+	}
+
+	// otherwise, we have to send a request to each potential interface.
+	const interfaces = Object.values(os.networkInterfaces());
+	const devices: Device[] = [];
+	for (const addresses of interfaces) {
+		if (!addresses) {
+			continue;
+		}
+		for (const address of addresses) {
+			// Right now we are assuming this should be an IPv4 address ... but I don't know if that's
+			// actually a good assumption.  We should listen for feedback on this one.
+			if (address.family === 'IPv4' && !address.internal) {
+				const broadcastAddress = calculateBroadcastAddress(
+					address.address,
+					address.netmask,
+				);
+				const localDevices = await sendDiscoveryRequest(
+					broadcastAddress,
+					timeout,
+				);
+				devices.push(...localDevices);
+			}
+		}
+	}
+	return devices;
+}
+
+/**
+ * Calculate the broadcast address for a given IP address and subnet mask.
+ *
+ * This will frequently return a value like `10.0.0.255` or `192.168.1.255`.
+ */
+function calculateBroadcastAddress(ip: string, subnetMask: string): string {
+	const ipParts = ip.split('.').map(Number);
+	const maskParts = subnetMask.split('.').map(Number);
+	const broadcastParts = ipParts.map(
+		(ipPart, i) => ipPart | (~maskParts[i] & 255),
+	);
+	return broadcastParts.join('.');
+}
+
+/**
+ * Send a discovery request to the given broadcast address and return the devices that respond.
+ */
+async function sendDiscoveryRequest(
+	broadcastAddress: string,
+	timeout: number,
+): Promise<Device[]> {
+	const socket = dgram.createSocket('udp4');
+	socket.bind(() => {
+		socket.setBroadcast(true);
+	});
 
 	// Discovery request message to find all Twinkly devices on the network:
 	// 1 byte 0x01 probably version
 	// 8 bytes as a string discover
 	const message = Buffer.from('1discover');
 
-	const socket = dgram.createSocket('udp4');
-	socket.bind(() => {
-		socket.setBroadcast(true);
-	});
-	return await new Promise<Device[]>((resolve, reject) => {
+	const devices = await new Promise<Device[]>((resolve, reject) => {
 		const devices: Device[] = [];
 		setTimeout(() => {
 			socket.close();
@@ -65,9 +118,13 @@ export async function discover(options?: DiscoverOptions) {
 				(err) => {
 					if (err) {
 						socket.close();
-						reject(err);
+						// This could fail for a variety of reasons. We should log and continue.
+						if (process.env.TWINKLY_DEBUG) {
+							console.error(err);
+						}
 					}
 				},
 			);
 	});
+	return devices;
 }
